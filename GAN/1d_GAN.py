065cd9e59da62b87113b59988917f66418b0296c
josjsjen <zhujie1207@gmail.com>
2018-05-14 22:38:36 -0400

    six commit

--- GAN/1d_GAN.py
+++ GAN/1d_GAN.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import seaborn as sns
+import matplotlib.mlab as mlab
 
 sns.set(color_codes=True)
 
@@ -29,13 +30,13 @@ class DataDistribution(object):
         samples.sort()
         return samples   
     
-# generator with noise  – the samples are first generated uniformly over a specified range, and then randomly perturbed.  
+# generator with noise  
 class GeneratorDistribution(object):
     def __init__(self, range):
         self.range = range
 
     def sample(self, N):
-        return np.linspace(-self.range, self.range, N) + np.random.random(N) * 0.01
+        return np.linspace(-self.range, self.range, N) + np.random.random(N) 
    
 
 #  linear transformation
@@ -69,23 +70,19 @@ def discriminator(input, hidden_size):
     h3 = tf.sigmoid(linear(h2, 1, 'd3')) #binary classification: sigmoid; multi-class use softmax
     return h3
 
-###################################
-#### create a TensorFlow Graph ####
-with tf.variable_scope('G'):
-    z = tf.placeholder(tf.float32, shape=(None, 1))
-    G = generator(z, hidden_size)
 
-with tf.variable_scope('D') as scope:
-    x = tf.placeholder(tf.float32, shape=(None, 1))
-    D1 = discriminator(x, hidden_size)
-    scope.reuse_variables()
-    D2 = discriminator(G, hidden_size)
+def optimizer(loss, var_list):
+    learning_rate = 0.001
+    step = tf.Variable(0, trainable=False)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(
+        loss,
+        global_step=step,
+        var_list=var_list
+    )
+    return optimizer
 
-loss_d = tf.reduce_mean(-tf.log(D1) - tf.log(1 - D2))
-loss_g = tf.reduce_mean(-tf.log(D2))
-
-
-#### define an optimizer
+'''
+## optimizer Option 2 : exponential_decay optimizer
 def optimizer(loss, var_list):
     initial_learning_rate = 0.005
     decay = 0.90
@@ -104,6 +101,53 @@ def optimizer(loss, var_list):
         var_list=var_list
     )
     return optimizer
+'''
+
+'''
+
+################################
+######### Visualize data #######
+x=data.sample(1000)
+num_bins = 100
+# the histogram of the data
+n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
+# add a 'best fit' line
+y = mlab.normpdf(bins, data.mu, data.sigma)
+plt.plot(bins, y, 'r--')
+plt.xlabel('raw data')
+plt.ylabel('Probability')
+
+# Tweak spacing to prevent clipping of ylabel
+plt.subplots_adjust(left=0.15)
+plt.show()
+
+'''
+###################################
+#### create a TensorFlow Graph ####
+
+num_steps=5000
+batch_size=8
+
+hidden_size=4
+
+data=DataDistribution()
+gen=GeneratorDistribution(10)
+
+
+
+with tf.variable_scope('G', reuse=True):
+    z = tf.placeholder(tf.float32, shape=(None, 1))
+    G = generator(z, hidden_size)
+
+with tf.variable_scope('D', reuse=True): # equal to with tf.variable_scope('D', reuse=True)
+    x = tf.placeholder(tf.float32, shape=(None, 1))
+    D1 = discriminator(x, hidden_size)
+    scope.reuse_variables()
+    D2 = discriminator(G, hidden_size)
+
+loss_d = tf.reduce_mean(-tf.log(D1) - tf.log(1 - D2))
+loss_g = tf.reduce_mean(-tf.log(D2))
+
 
 vars = tf.trainable_variables()
 d_params = [v for v in vars if v.name.startswith('D/')]
@@ -113,20 +157,17 @@ g_params = [v for v in vars if v.name.startswith('G/')]
 opt_d = optimizer(loss_d, d_params)
 opt_g = optimizer(loss_g, g_params)
 
-################################
-######### define variable ######
-num_steps=1000
-batch_size=500
 
-data=DataDistribution()
-gen=GeneratorDistribution()
 
 ################################
 ######### train models #########
+
 with tf.Session() as session:
-    tf.initialize_all_variables().run()
+    #tf.initialize_all_variables().run()
+    tf.global_variables_initializer().run()
 
     for step in xrange(num_steps):
+        # update discriminator
         x = data.sample(batch_size)
         z = gen.sample(batch_size)
         
@@ -134,7 +175,7 @@ with tf.Session() as session:
             x: np.reshape(x, (batch_size, 1)),
             z: np.reshape(z, (batch_size, 1))
         })
-
+    '''
         # update generator
         z = gen.sample(batch_size)
         loss_g, _ =session.run([loss_g, opt_g], {
@@ -144,11 +185,10 @@ with tf.Session() as session:
         if step % 10 == 0:
                 print('{}: {:.4f}\t{:.4f}'.format(step, loss_d, loss_g))
 
-                
+   '''            
                 
 ####################################################
 ######## PART 2: minibatch discrimination ##########
 ## Problem: If the generator just produces the mean value of the real data in this simple example, 
 ## then it is going to be quite likely to fool the discriminator.
 ## Solution: Give the discriminator the ability to examine multiple examples at once -- minibatch discrimination.
-
